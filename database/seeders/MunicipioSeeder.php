eb9a583f6e303e5ffe894693ac4443e9e6326bd6
HeraldCNP <Heraldcnp@gmail.com>
2025-03-28 17:32:03 -0400

    back V1

--- database/seeders/MunicipioSeeder.php
+++ database/seeders/MunicipioSeeder.php
@@ -55,108 +55,178 @@ class MunicipioSeeder extends Seeder
             ],
             [
                 'nombre' => 'Colcha "K"',
+                'slug' => Str::slug('Colcha "K"'),
+                'user_id' => 1, 
             ],
             [
                 'nombre' => 'LLica',
+                'slug' => Str::slug('LLica'),
+                'user_id' => 1,
             ],
             [
                 'nombre' => 'Mojinete',
+                'slug' => Str::slug('Mojinete'),
+                'user_id' => 1,
             ],
             [
                 'nombre' => 'Porco',
+                'slug' => Str::slug('Porco'),
+                'user_id' => 1,
             ],
             [
                 'nombre' => 'San Agustin',
+                'slug' => Str::slug('San Agustin'),
+                'user_id' => 1,
             ],
             [
                 'nombre' => 'San Antonio de Esmoruco',
+                'slug' => Str::slug('San Antonio de Esmoruco'),
+                'user_id' => 1,
             ],
             [
                 'nombre' => 'San Pablo de Lipez',
+                'slug' => Str::slug('San Pablo de Lipez'),
+                'user_id' => 1,
             ],
             [
                 'nombre' => 'San Pedro de Quemes',
+                'slug' => Str::slug('San Pedro de Quemes'),
+                'user_id' => 1,
             ],
             [
                 'nombre' => 'Tahua',
+                'slug' => Str::slug('Tahua'),
+                'user_id' => 1,
             ],
             [
                 'nombre' => 'Tomave',
+                'slug' => Str::slug('Tomave'),
+                'user_id' => 1,
             ],
             [
                 'nombre' => 'Uyuni',
+                'slug' => Str::slug('Uyuni'),
+                'user_id' => 1,
             ],
             [
                 'nombre' => 'Atocha',
+                'slug' => Str::slug('Atocha'),
+                'user_id' => 1,
             ],
             [
                 'nombre' => 'Cotagaita',
+                'slug' => Str::slug('Cotagaita'),
+                'user_id' => 1,
             ],
             [
                 'nombre' => 'Tupiza',
+                'slug' => Str::slug('Tupiza'),
+                'user_id' => 1,
             ],
             [
                 'nombre' => 'Villazon',
+                'slug' => Str::slug('Villazon'),
+                'user_id' => 1,
             ],
             [
                 'nombre' => 'Vitichi',
+                'slug' => Str::slug('Vitichi'),
+                'user_id' => 1,
             ],
             [
                 'nombre' => 'Uncia',
+                'slug' => Str::slug('Uncia'),
+                'user_id' => 1,
             ],
             [
                 'nombre' => 'Caripuyo',
+                'slug' => Str::slug('Caripuyo'),
+                'user_id' => 1,
             ],
             [
                 'nombre' => 'Chayanta',
+                'slug' => Str::slug('Chayanta'),
+                'user_id' => 1,
             ],
             [
                 'nombre' => 'Chuquihuta',
+                'slug' => Str::slug('Chuquihuta'),
+                'user_id' => 1,
             ],
             [
                 'nombre' => 'Colquechaca',
+                'slug' => Str::slug('Colquechaca'),
+                'user_id' => 1,
             ],
             [
                 'nombre' => 'LLallagua',
+                'slug' => Str::slug('LLallagua'),
+                'user_id' => 1,
             ],
             [
                 'nombre' => 'Pocoata',
+                'slug' => Str::slug('Pocoata'),
+                'user_id' => 1,
             ],
             [
                 'nombre' => 'Ravelo',
+                'slug' => Str::slug('Ravelo'),
+                'user_id' => 1,
             ],
             [
                 'nombre' => 'San Pedro de Macha',
+                'slug' => Str::slug('San Pedro de Macha'),
+                'user_id' => 1,
             ],
             [
                 'nombre' => 'Sacaca',
+                'slug' => Str::slug('Sacaca'),
+                'user_id' => 1,
             ],
             [
                 'nombre' => 'Acasio',
+                'slug' => Str::slug('Acasio'),
+                'user_id' => 1,
             ],
             [
                 'nombre' => 'Belen de Urmiri',
+                'slug' => Str::slug('Belen de Urmiri'),
+                'user_id' => 1,
             ],
             [
                 'nombre' => 'Caiza "D"',
+                'slug' => Str::slug('Caiza "D"'),
+                'user_id' => 1,
             ],
             [
                 'nombre' => 'Ckochas',
+                'slug' => Str::slug('Ckochas'),
+                'user_id' => 1,
             ],
             [
                 'nombre' => 'Potosí',
+                'slug' => Str::slug('Potosí'),
+                'user_id' => 1,
             ],
             [
                 'nombre' => 'Puna',
+                'slug' => Str::slug('Puna'),
+                'user_id' => 1,
             ],
             [
                 'nombre' => 'Tacobamba',
+                'slug' => Str::slug('Tacobamba'),
+                'user_id' => 1,
             ],
             [
                 'nombre' => 'Tinguipaya',
+                'slug' => Str::slug('Tinguipaya'),
+                'user_id' => 1,
             ],
             [
                 'nombre' => 'Yocalla',
+                'slug' => Str::slug('Yocalla'),
+                'user_id' => 1,
             ],
             // Añade más municipios aquí...
         ];
